9f77bca194072279780bb80469dec6500ecca7e1
HelioMelo <Helio_melo_@hotmail.com>
2024-11-14 15:42:01 -0300

    GET LISTS

--- src/main/java/com/devsuperior/dslist/dto/GameDTO.java
+++ src/main/java/com/devsuperior/dslist/dto/GameDTO.java
@@ -1,4 +1,6 @@
-package com.devsuperior.dslist.services;
+package com.devsuperior.dslist.dto;
+
+
 
 import org.springframework.beans.BeanUtils;
 
--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
