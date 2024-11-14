d7910638ed464495bfe31e4d901596e443ccaf14
HelioMelo <Helio_melo_@hotmail.com>
2024-11-14 12:56:35 -0300

    Game repository, service, controller

---
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -0,0 +1,50 @@
+package com.devsuperior.dslist.dto;
+
+import com.devsuperior.dslist.entities.Game;
+
+public class GameMinDTO {
+
+	private Long id;
+	private String title;
+	private Double score;
+	private String imgUrl;
+	private String shortDescription;
+	
+	public GameMinDTO() {
+		
+	}
+
+	public GameMinDTO(Game entity) {
+		id = entity.getId();
+		title = entity.getTitle();
+		score = entity.getScore();
+		imgUrl = entity.getImgURL();
+		shortDescription = entity.getShortDescription();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public Double getScore() {
+		return score;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public String getShortDescription() {
+		return shortDescription;
+	}
+	
+	
+	
+	
+
+
+}
